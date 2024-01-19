f9eaf79c6a926e2bf995d9950e423f2e736a03a8
André Saxton <andre_saxton@hotmail.com>
2024-01-19 17:17:39 -0300

    Testes para sua aplicação

--- App.js
+++ App.js
@@ -5,15 +5,21 @@ import { KeyboardAvoidingView, SafeAreaView, StatusBar, StyleSheet } from 'react
 import AppRotas from './src/rotas/AppRotas';
 
 export default function App() {
+  // return (
+  //   <SafeAreaView style={estilos.container}>
+  //     <StatusBar />
+  //     <KeyboardAvoidingView 
+  //       style={estilos.teclado}
+  //       keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 45}
+  //       behavior={Platform.OS === "ios" ? "padding" : "height"}>
+  //       <AppRotas />
+  //     </KeyboardAvoidingView>
+  //   </SafeAreaView>
+  // );
   return (
     <SafeAreaView style={estilos.container}>
       <StatusBar />
-      <KeyboardAvoidingView 
-        style={estilos.teclado}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 45}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}>
         <AppRotas />
-      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
